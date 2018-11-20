fcd03a458f00ef367687e539b41252cbbff1813a
ryo nagata <ryo.nagata.utokyo@gmail.com>
2018-11-20 15:41:09 +0900

    first commit

---
+++ src/app/infrastructure/router.go
@@ -0,0 +1,22 @@
+package infrastructure
+
+import (
+	"CAwithGO/src/app/interfaces/controllers"
+	gin "gopkg.in/gin-gonic/gin.v1"
+
+)
+
+
+var Router *gin.Engine
+
+func init() {
+	router := gin.Default()
+
+	userController := controllers.NewUserController(NewSqlHandler())
+
+	router.POST("/users", func(c *gin.Context) { userController.Create(c) })
+	router.GET("/users", func(c *gin.Context) { userController.Index(c) })
+	router.GET("/users/:id", func(c *gin.Context) { userController.Show(c) })
+
+	Router = router
+}
---
+++ src/app/server.go
@@ -0,0 +1,7 @@
+package main
+
+import "CAwithGO/src/app/infrastructure"
+
+func main() {
+	infrastructure.Router.Run()
+}
